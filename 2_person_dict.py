842cc30cb5c8f4604388df3d27244676089025a1
Danny <danny@email.com>
2022-09-13 12:14:54 -0500

    class 9/13

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,3 +5,18 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
+
+print(person)
+
+print(person['children'][1])
+
+print(person["pets"]["cat"])
+
+for i in person["children"]:
+    print(i)
+
+for i in person["pets"]:
+    print(person["pets"][i])
+
+for i in person["pets"].values():
+    print(i)
